1e3a3f4913f1c968c3aba2f76ceb8c5bc028f181
Brian Messick <bmessick12@gmail.com>
2021-07-27 21:12:10 -0600

    i give up

--- src/App.js
+++ src/App.js
@@ -11,13 +11,14 @@ import ParentComponent from './components/ParentComponent'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
 import EventBind from './components/EventBind'
+import NameList from './components/NameList'
 
 
 
 function App() {
   return (
     <div className="App">
-     
+     <NameList />
        
 <FunctionClick />
 <ClassClick />
---
+++ src/components/NameList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function NameList() {
+
+    const persons = [
+    {
+        id: 1,
+        name: 'Brian',
+        age: 30,
+        skill: 'time travel'
+    },
+    {
+        id: 2,
+        name: 'Max',
+        age: 30,
+        skill: 'Invulnerablilty'
+    },
+    {
+        id: 3,
+        name: 'Emilia',
+        age: 28,
+        skill: 'Teleportation'
+    }
+    ]
+
+const personList = persons.map(person => <Person person={person} />)
+    return <div>{personList}</div>
+}
+export default NameList
